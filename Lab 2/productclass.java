6d64cd34a33f520f445c087c163d1b75394f7d66
MM.Abdullah <abdullahrauf245@gmail.com>
2026-03-31 20:38:29 +0500

    Lab 2

---
+++ Lab 2/productclass.java
@@ -0,0 +1,13 @@
+
+
+public class productclass {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int product = num1 * num2;
+
+        System.err.println("The product of " + num1 + " and " + num2 + " is: " + product);
+
+    }
+    
+}
